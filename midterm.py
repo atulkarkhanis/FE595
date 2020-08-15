5419f0d62ad1394ef50c227cb05b73b0c80c9dc3
atulkarkhanis <atul.karkhanis@gmail.com>
2020-08-15 13:21:45 -0400

    updates added on 8/15

--- midterm.py
+++ midterm.py
@@ -1,11 +1,23 @@
 from flask import Flask, redirect,url_for,render_template,request
-import nltk.sentiment.vader as nk
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sid = SentimentIntensityAnalyzer()
+
+import spacy
+sp = spacy.load("en_core_web_sm")
+
 
 def analyzer(sentence):
-    nkobj = nk.SentimentIntensityAnalyzer()
-    results = nkobj.polarity_scores(sentence)
+    results = sid.polarity_scores(sentence)
     print(results)
     return results
+
+def exspacy(text):
+    spacydoc = sp(text)
+    doc = spacydoc.ents
+    print(doc)
+    return doc
 
 app = Flask(__name__)
 @app.route("/")
@@ -14,15 +26,38 @@ def home():
 
 @app.route("/",methods=["POST","GET"])
 def getsenti():
+    newresult = []
     if request.method == "POST":
         sentence = request.form["thought"]
-        response = analyzer(sentence)
-        return redirect(url_for("results",usr=sentence))
+        newresult= analyzer(sentence)
+        sentiment = newresult["compound"]
+        print(sentiment)
+        if sentiment >0.5:
+            return redirect(url_for("results", usr="Sounds Positive!"))
+        else:
+            return redirect(url_for("results", usr="Just another day"))
+        #return render_template("results.html", usr=sentence)
     else:
         return render_template("index.html")
-@app.route("/results")
-def user(usr):
+       # return render_template("index.html")
+@app.route("/<usr>")
+def results(usr):
     return render_template("results.html", usr=usr)
+
+@app.route("/pos.html",methods=["POST","GET"])
+def tokens():
+    if request.method == "POST":
+        text = request.form["content"]
+        doc = exspacy(text)
+        return redirect(url_for("spdoc", doc=doc))
+
+    else:
+        return render_template("pos.html")
+
+
+@app.route("/<doc>")
+def spdoc(doc):
+    return render_template("results.html", doc=doc)
 
 if __name__ == "__main__":
     app.run()
